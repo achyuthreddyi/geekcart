973d0aac5de88fe7c9a4298c5248527858f97d32
achyuth reddy <ncsachyuth@gmail.com>
2020-12-06 14:52:30 +0530

    created all the middlewares for the authentication

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,6 +1,8 @@
 const { validationResult } = require('express-validator')
 const { generateToken } = require('../config/generateToken')
 const User = require('./userModel')
+const expresJwt = require('express-jwt')
+const router = require('./userRoute')
 
 // database functionalities
 
@@ -43,6 +45,25 @@ const getUser = async user => {
   }
 }
 
+const checkPassword = async user => {
+  const { email, password } = user
+  try {
+    const user = await User.findOne({ email })
+    if (!user.authenticate(password)) {
+      return {
+        error: 'user email and password did not match'
+      }
+    } else {
+      return user
+    }
+  } catch (err) {
+    return {
+      error: 'user not being able to fetch from the database',
+      err
+    }
+  }
+}
+
 exports.signUp = async (req, res) => {
   const result = await registerUser(req.body)
 
@@ -61,19 +82,68 @@ exports.signUp = async (req, res) => {
 }
 
 exports.signIn = async (req, res) => {
-  const result = await getUser(req.body)
+  const { email, password } = req.body
+
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      error: errors.array()[0].msg
+    })
+  }
+
+  const result = await checkPassword({ email, password })
+
   if (!result.error) {
     const payload = {
       _id: result._id
     }
 
     const token = generateToken(payload)
-    res.status(200).json(token)
+    const { _id, name, email, role } = result
+    res.cookie('token', token, { expire: new Date() + 99999 })
+    res.status(200).json({
+      token,
+      user: {
+        _id,
+        name,
+        email,
+        role
+      }
+    })
   } else {
     res.status(400).send(result)
   }
 }
 
 exports.signOut = (req, res) => {
-  res.json('hi')
+  res.clearCookie('token')
+  res.json({
+    message: 'User Signout successfully'
+  })
+}
+
+// protected routes
+exports.isSignedIn = expresJwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'auth'
+})
+
+// custom middlewares
+
+exports.isAuthenticated = (req, res, next) => {
+  const checker = req.profile && req.auth && req.profile._id === req.auth._id
+  if (!checker) {
+    return res.status(403).json({
+      error: 'Access denied'
+    })
+  }
+  next()
+}
+
+exports.isAdmin = (req, res, next) => {
+  if (req.profile.role === 0) {
+    return res.status(403).json({
+      error: 'You are not an admin'
+    })
+  }
 }
--- backend/users/userRoute.js
+++ backend/users/userRoute.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const { check } = require('express-validator')
-const { signOut, signUp, signIn } = require('./userController')
+const { signOut, signUp, signIn, isSignedIn } = require('./userController')
 const router = express.Router()
 
-router.get('/signout', signOut)
 router.post(
   '/signup',
   [
@@ -16,6 +15,22 @@ router.post(
   ],
   signUp
 )
-router.post('/signin', signIn)
+router.post(
+  '/signin',
+  [
+    check('password')
+      .isLength({ min: 3 })
+      .withMessage('must be at least 3 chars long'),
+
+    check('email')
+      .isEmail()
+      .withMessage('must be a proper email')
+  ],
+  signIn
+)
+router.post('/signout', signOut)
+router.get('/test', isSignedIn, (req, res) => {
+  res.json(req.auth)
+})
 
 module.exports = router
